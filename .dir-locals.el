6b18fc1b5735a0c9063c6b9d48fc824c72a95c4e
Johann Rosain <johann.rosain@ens-lyon.org>
2025-10-19 18:00:11 +0200

    Add: auto-prettify notations for emacs users

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((coq-mode . ((company-coq-dir-local-symbols . (("\\to" . ?→))))))
