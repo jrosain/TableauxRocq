a5afb6e93b341d129494e5e2efd34aa5860fdf5d
Johann Rosain <37924288+jrosain@users.noreply.github.com>
2026-01-07 09:29:40 +0100

    CI: add build on pull requests & push to `master` (#5)
    Add a CI script that makes use of the nix reproducible environment to
    build the library on pull requests.
    Fix the nixpkgs version to match between `rocq-core` and `rocq-stdlib`.
    
    Closes: #4 .

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/25.11.tar.gz";
-  sha256 = "1zn1lsafn62sz6azx6j735fh4vwwghj8cc9x91g5sx2nrg23ap9k";
+  url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/25.05.tar.gz";
+  sha256 = "1915r28xc4znrh2vf4rrjnxldw2imysz819gzhk9qlrkqanmfsxd";
 })
